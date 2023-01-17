dee3ebf5eb3ade217577bdffbdc5913127968a22
James Bowles <jamesrbowles@gmail.com>
2023-01-17 14:34:49 +0100

    completed func

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { marked } from "marked";
 import { useState } from "react";
 
 // css
@@ -5,14 +6,37 @@ import "./App.css";
 
 // components
 import Editor from "./components/Editor/Editor";
-import Previewer from "./components/Previewer/Previewer";
 
 function App() {
-  const [text, setText] = useState("");
+  const [text, setText] = useState(`
+  # H1
+  ## H2
+  My favorite search engine is [Duck Duck Go](https://duckduckgo.com).
+  \`code\`
+  \`\`\`
+  {
+    "firstName": "John",
+    "lastName": "Smith"
+  }
+  \`\`\`
+  - First item
+  - Second item
+  - Third item
+  - Fourth item
+
+  > Dorothy followed her through many of the beautiful rooms in her castle.
+
+  ![The San Juan Mountains are beautiful!](/assets/images/san-juan-mountains.jpg "San Juan Mountains")
+
+  **bold text**
+  `);
+
+  marked.setOptions({
+    breaks: true,
+  });
   return (
     <div className="container">
       <Editor text={text} setText={setText} />
-      <Previewer text={text} />
     </div>
   );
 }
--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,19 +1,41 @@
 // css
 import "./Editor.css";
+import "./Previewer.css";
+
+//markdown
+import { marked } from "marked";
+
+// icons
+import { FaExpandArrowsAlt } from "react-icons/fa";
 
 const Editor = ({ text, setText }) => {
   return (
-    <div className="editor-section">
-      <div className="navbar">
-        <h3>Editor</h3>
-        {/*         add an expand icon here
-         */}
-        <span>expand icon</span>
+    <div className="container-section">
+      <div>
+        <div className="edit-navbar">
+          <h3>Editor</h3>
+          {/*         add an expand icon here
+           */}
+          <FaExpandArrowsAlt className="expand-icon" />
+        </div>
+        <textarea
+          id="editor"
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+        ></textarea>
+        <div className="previewer-section">
+          <div className="prev-navbar">
+            <h3>Previewer</h3>
+            {/*         add an expand icon here
+             */}
+            <FaExpandArrowsAlt className="expand-icon" />
+          </div>
+          <div
+            id="previewer"
+            dangerouslySetInnerHTML={{ __html: marked(text) }}
+          ></div>
+        </div>
       </div>
-      <textarea
-        id="editor"
-        onChange={(e) => setText(e.target.value)}
-      ></textarea>
     </div>
   );
 };
